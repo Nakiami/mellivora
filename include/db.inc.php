c2329be6e21322b86829a688c8be389001dd5e3f
Nakiami <contact@greyboxconcepts.com.au>
2013-07-18 21:55:45 +1000

    DB engine as config option

--- include/db.inc.php
+++ include/db.inc.php
@@ -4,6 +4,7 @@ if (!defined('IN_FILE')) {
     exit(); // TODO report error
 }
 
+define('DB_ENGINE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'mellivora');
 define('DB_USER', 'root');
