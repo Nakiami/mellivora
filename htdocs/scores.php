56e5b9e82e1600facd5c8b171aa7c7866745b6a6
Nakiami <contact@greyboxconcepts.com.au>
2013-09-21 20:10:40 +1000

    HS have their own scoreboard

--- htdocs/scores.php
+++ htdocs/scores.php
@@ -68,7 +68,68 @@ echo '
 echo '
       </tbody>
     </table>
+    ';
 
+sectionHead('HS Scoreboard');
+
+echo '
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Team name</th>
+          <th>Points</th>
+        </tr>
+      </thead>
+      <tbody>
+     ';
+
+$chal_stmt = $db->query('
+    SELECT
+    u.id AS user_id,
+    u.team_name,
+    u.type,
+    SUM(c.points) AS score,
+    MAX(s.added) AS tiebreaker
+    FROM users AS u
+    LEFT JOIN submissions AS s ON u.id = s.user_id AND s.correct = 1
+    LEFT JOIN challenges AS c ON c.id = s.challenge
+    WHERE u.class = '.CONFIG_UC_USER.' AND u.type = "hs"
+    GROUP BY u.id
+    ORDER BY score DESC, tiebreaker ASC
+');
+
+$i = 1;
+while($place = $chal_stmt->fetch(PDO::FETCH_ASSOC)) {
+
+    echo '
+    <tr>
+      <td>',number_format($i),'</td>
+      <td>';
+    if ($_SESSION['id']) {
+
+        echo '<a href="user?id=',htmlspecialchars($place['user_id']),'">',
+        ($place['user_id'] == $_SESSION['id'] ? '<span class="label label-info">'.htmlspecialchars($place['team_name']).'</span>' : htmlspecialchars($place['team_name'])),
+        '</a>';
+    }
+    else {
+        echo htmlspecialchars($place['team_name']);
+    }
+    echo '
+      </td>
+      <td>',number_format($place['score']),'</td>
+    </tr>
+';
+
+    $i++;
+}
+
+echo '
+      </tbody>
+    </table>
+';
+
+echo '
     </div>  <!-- / span6 -->
 
     <div class="span6">
