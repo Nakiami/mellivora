20c8a8e8f2574c707da5c9e45f55e8668e1cb355
Nakiami <contact@greyboxconcepts.com.au>
2013-09-21 20:07:15 +1000

    Added config option to disable signups

--- htdocs/login.php
+++ htdocs/login.php
@@ -45,33 +45,39 @@ echo '
     <input type="hidden" name="action" value="login" />
     <button class="btn btn-primary" type="submit">Sign in</button> <a href="recover_password.php">I\'ve forgotten my password</a>
 </form>
-
-<form method="post" class="form-signin">
-    <h2 class="form-signin-heading">or, register a team</h2>
-    <p>
-        Your team shares one account.
-        ',(CONFIG_ACCOUNTS_EMAIL_PASSWORD_ON_SIGNUP ? 'An confirmation email containing your password will be sent to the chosen address.' : ''),'
-    </p>
-    <input name="',md5(CONFIG_SITE_NAME.'USR'),'" type="text" class="input-block-level" placeholder="Email address">
-    <input name="',md5(CONFIG_SITE_NAME.'PWD'),'" type="password" class="input-block-level" placeholder="Password">
-    <input name="',md5(CONFIG_SITE_NAME.'TEAM'),'" type="text" class="input-block-level" placeholder="Team name" maxlength="',CONFIG_MAX_TEAM_NAME_LENGTH,'">
-
-    <select name="type" class="input-block-level">
-        <option>-- Please select team type. If your team is mixed, select the highest institution --</option>
-        <option value="uni">University team</option>
-        <option value="hs">High school team</option>
-        <option value="tafe">TAFE team</option>
-    </select>
-    ';
-
-    if (CONFIG_RECAPTCHA_ENABLE) {
-        displayCaptcha();
-    }
-
-    echo '
-    <input type="hidden" name="action" value="register" />
-    <button class="btn btn-primary" type="submit">Register team</button>
-</form>
 ';
+
+if (CONFIG_ACCOUNTS_SIGNUP_ALLOWED) {
+    echo '
+    <form method="post" class="form-signin">
+        <h2 class="form-signin-heading">or, register a team</h2>
+        <p>
+            Your team shares one account.
+            ',(CONFIG_ACCOUNTS_EMAIL_PASSWORD_ON_SIGNUP ? 'An confirmation email containing your password will be sent to the chosen address.' : ''),'
+        </p>
+        <input name="',md5(CONFIG_SITE_NAME.'USR'),'" type="text" class="input-block-level" placeholder="Email address">
+        <input name="',md5(CONFIG_SITE_NAME.'PWD'),'" type="password" class="input-block-level" placeholder="Password">
+        <input name="',md5(CONFIG_SITE_NAME.'TEAM'),'" type="text" class="input-block-level" placeholder="Team name" maxlength="',CONFIG_MAX_TEAM_NAME_LENGTH,'">
+
+        <select name="type" class="input-block-level">
+            <option>-- Please select team type. If your team is mixed, select the highest institution --</option>
+            <option value="uni">University team</option>
+            <option value="hs">High school team</option>
+            <option value="tafe">TAFE team</option>
+        </select>
+        ';
+
+        if (CONFIG_RECAPTCHA_ENABLE) {
+            displayCaptcha();
+        }
+
+        echo '
+        <input type="hidden" name="action" value="register" />
+        <button class="btn btn-primary" type="submit">Register team</button>
+    </form>
+    ';
+} else {
+    echo '<i>Registration is currently closed.</i>';
+}
 
 foot();
