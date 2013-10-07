f08f80304f9d69d537b00ae819fdc963241a0ae6
Nakiami <contact@greyboxconcepts.com.au>
2013-10-07 18:21:16 +1100

    Fixed bug in SQL on hints, and added caching

--- htdocs/hints.php
+++ htdocs/hints.php
@@ -5,49 +5,55 @@ require('../include/general.inc.php');
 
 enforce_authentication();
 
-head('Hints');
-section_head('Hints');
+$cache = new Cache_Lite_Output(array('cacheDir'=>CONFIG_CACHE_PATH, 'lifeTime'=>CONFIG_CACHE_TIME_HINTS));
+if (!($cache->start('hints'))) {
 
-echo '
-    <table id="files" class="table table-striped table-hover">
-      <thead>
-        <tr>
-          <th>Category</th>
-          <th>Challenge</th>
-          <th>Added</th>
-          <th>Hint</th>
-        </tr>
-      </thead>
-      <tbody>
-    ';
+    head('Hints');
+    section_head('Hints');
 
-$stmt = $db->query('
-    SELECT
-    h.id,
-    h.added,
-    h.body,
-    c.title,
-    ca.title AS category_title
-    FROM hints AS h
-    LEFT JOIN challenges AS c ON c.id = h.challenge
-    LEFT JOIN catefories AS ca ON ca.id = c.category
-    WHERE c.available_from < UNIX_TIMESTAMP() AND c.available_until > UNIX_TIMESTAMP() AND h.visible = 1
-    ORDER BY h.id DESC
-');
-while($hint = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo '
-    <tr>
-        <td>',htmlspecialchars($hint['category_title']),'</td>
-        <td>',htmlspecialchars($hint['title']),'</td>
-        <td>',get_time_elapsed($hint['added']),' ago</td>
-        <td>',htmlspecialchars($hint['body']),'</td>
-    </tr>
-    ';
+        <table id="files" class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>Category</th>
+              <th>Challenge</th>
+              <th>Added</th>
+              <th>Hint</th>
+            </tr>
+          </thead>
+          <tbody>
+        ';
+
+    $stmt = $db->query('
+        SELECT
+        h.id,
+        h.added,
+        h.body,
+        c.title,
+        ca.title AS category_title
+        FROM hints AS h
+        LEFT JOIN challenges AS c ON c.id = h.challenge
+        LEFT JOIN categories AS ca ON ca.id = c.category
+        WHERE c.available_from < UNIX_TIMESTAMP() AND c.available_until > UNIX_TIMESTAMP() AND h.visible = 1
+        ORDER BY h.id DESC
+    ');
+    while($hint = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '
+        <tr>
+            <td>',htmlspecialchars($hint['category_title']),'</td>
+            <td>',htmlspecialchars($hint['title']),'</td>
+            <td>',get_time_elapsed($hint['added']),' ago</td>
+            <td>',htmlspecialchars($hint['body']),'</td>
+        </tr>
+        ';
+    }
+
+    echo '
+          </tbody>
+        </table>
+         ';
+
+    foot();
+
+    $cache->end();
 }
-
-echo '
-      </tbody>
-    </table>
-     ';
-
-foot();
