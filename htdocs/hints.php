8906bb19d27db18417c004f2cf3a3ea04df93b28
Nakiami <contact@greyboxconcepts.com.au>
2014-04-18 13:53:12 +1000

    Use db_query instead of calling PDO directly

--- htdocs/hints.php
+++ htdocs/hints.php
@@ -9,6 +9,24 @@ head('Hints');
 $cache = new Cache_Lite_Output(array('cacheDir'=>CONFIG_PATH_CACHE, 'lifeTime'=>CONFIG_CACHE_TIME_HINTS));
 if (!($cache->start('hints'))) {
 
+    $hints = db_query('
+        SELECT
+        h.id,
+        h.added,
+        h.body,
+        c.title,
+        ca.title AS category_title
+        FROM hints AS h
+        LEFT JOIN challenges AS c ON c.id = h.challenge
+        LEFT JOIN categories AS ca ON ca.id = c.category
+        WHERE c.available_from < UNIX_TIMESTAMP() AND c.available_until > UNIX_TIMESTAMP() AND h.visible = 1
+        ORDER BY h.id DESC
+    ');
+
+    if (!sizeof($hints)) {
+        message_generic("Hints", "No hints have been made available yet.", false);
+    }
+
     section_head('Hints');
 
     echo '
@@ -24,20 +42,7 @@ if (!($cache->start('hints'))) {
           <tbody>
         ';
 
-    $stmt = $db->query('
-        SELECT
-        h.id,
-        h.added,
-        h.body,
-        c.title,
-        ca.title AS category_title
-        FROM hints AS h
-        LEFT JOIN challenges AS c ON c.id = h.challenge
-        LEFT JOIN categories AS ca ON ca.id = c.category
-        WHERE c.available_from < UNIX_TIMESTAMP() AND c.available_until > UNIX_TIMESTAMP() AND h.visible = 1
-        ORDER BY h.id DESC
-    ');
-    while($hint = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    foreach ($hints as $hint) {
         echo '
         <tr>
             <td>',htmlspecialchars($hint['category_title']),'</td>
