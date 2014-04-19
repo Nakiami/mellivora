16f50d6c4aa54d01e6b2e853c8f46148c6dbed22
Nakiami <contact@greyboxconcepts.com.au>
2014-04-19 12:57:07 +1000

    Nicer challenge page

--- htdocs/challenge.php
+++ htdocs/challenge.php
@@ -26,10 +26,6 @@ if (!($cache->start('challenge_' . $_GET['id']))) {
         array('id' => $_GET['id'])
     );
 
-    if (!count($submissions)) {
-        message_generic("Submissions", "This challenge has not yet been solved by any teams!", false);
-    }
-
     $challenge = db_query('
         SELECT
            ch.title,
@@ -37,44 +33,51 @@ if (!($cache->start('challenge_' . $_GET['id']))) {
            ca.title AS category_title
         FROM challenges AS ch
         LEFT JOIN categories AS ca ON ca.id = ch.category
-        WHERE ch.id=:id',
+        WHERE ch.id = :id',
         array('id'=>$_GET['id']),
         false
     );
 
     section_head($challenge['title']);
 
-    $user_count = db_query('SELECT COUNT(*) AS num FROM users WHERE competing = 1', null, false);
+    $num_correct_solves = count($submissions);
 
-    echo 'This challenge has been solved by ', (number_format(((count($submissions) / $user_count['num']) * 100), 1)), '% of users.';
-
-    echo '
-   <table class="table table-striped table-hover">
-   <thead>
-   <tr>
-     <th>Position</th>
-     <th>Team</th>
-     <th>Solved</th>
-   </tr>
-   </thead>
-   <tbody>
-   ';
-    $i = 1;
-    foreach ($submissions as $submission) {
-        echo '
-          <tr>
-            <td>', number_format($i), ' ', get_position_medal($i), '</td>
-            <td><a href="user.php?id=', htmlspecialchars($submission['user_id']), '">', htmlspecialchars($submission['team_name']), '</a></td>
-            <td>', time_elapsed($submission['added'], $submission['available_from']), ' after release, ', time_elapsed($submission['added']), ' ago (', date_time($submission['added']), ')</td>
-          </tr>
-          ';
-        $i++;
+    if (!$num_correct_solves) {
+        echo 'This challenge has not yet been solved by any teams.';
     }
 
-    echo '
-   </tbody>
-   </table>
-     ';
+    else {
+        $user_count = db_query('SELECT COUNT(*) AS num FROM users WHERE competing = 1', null, false);
+        echo 'This challenge has been solved by ', (number_format((($num_correct_solves / $user_count['num']) * 100), 1)), '% of users.';
+
+        echo '
+       <table class="table table-striped table-hover">
+       <thead>
+       <tr>
+         <th>Position</th>
+         <th>Team</th>
+         <th>Solved</th>
+       </tr>
+       </thead>
+       <tbody>
+       ';
+        $i = 1;
+        foreach ($submissions as $submission) {
+            echo '
+              <tr>
+                <td>', number_format($i), ' ', get_position_medal($i), '</td>
+                <td><a href="user.php?id=', htmlspecialchars($submission['user_id']), '">', htmlspecialchars($submission['team_name']), '</a></td>
+                <td>', time_elapsed($submission['added'], $submission['available_from']), ' after release, ', time_elapsed($submission['added']), ' ago (', date_time($submission['added']), ')</td>
+              </tr>
+              ';
+            $i++;
+        }
+
+        echo '
+       </tbody>
+       </table>
+         ';
+    }
 
     $cache->end();
 }
