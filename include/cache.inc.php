9656ba92d78f4e566f58f0abd4b31f885c92e92c
Nakiami <contact@greyboxconcepts.com.au>
2014-04-20 14:50:38 +1000

    Do not write cache if lifetime is zero

--- include/cache.inc.php
+++ include/cache.inc.php
@@ -7,10 +7,19 @@ $caches = array();
 function cache_start ($identifier, $lifetime) {
     global $caches;
 
+    // if lifetime is zero, we don't perform caching.
+    // by returning true, we signal that content needs to be recreated
+    if (!$lifetime) {
+        return true;
+    }
+
+    // if no caching object exists for this identifier, create it
     if (empty($caches[$identifier])) {
         $caches[$identifier] = new Cache_Lite_Output(array('cacheDir'=>CONFIG_PATH_CACHE, 'lifeTime'=>$lifetime));
     }
 
+    // return true if cache has expired, and we need to recreate content
+    // return false if cache is still valid
     return !($caches[$identifier]->start($identifier));
 }
 
