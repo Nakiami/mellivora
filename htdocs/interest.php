668a63c7a23dfea863c44d4c1a135fcc01c5367a
Nakiami <contact@greyboxconcepts.com.au>
2014-04-20 13:22:14 +1000

    Minor general improvements and cleanups

--- htdocs/interest.php
+++ htdocs/interest.php
@@ -7,9 +7,9 @@ prefer_ssl();
 head('Register interest');
 
 section_head('Register interest');
-message_inline_bland('The Mellivora team are likely to run more CTFs in the future. These will most likely be open to the public.
-                      Input your email below if you\'re interested in hearing from us about future competitions.
-                      We won\'t spam you. Your email address won\'t be shared with third parties.');
+message_inline_bland("The Mellivora team are likely to run more CTFs in the future. These will most likely be open to the public.
+                      Input your email below if you're interested in hearing from us about future competitions.
+                      We won't spam you. Your email address won't be shared with third parties.");
 
 form_start('actions/interest','form-signin');
 echo '
