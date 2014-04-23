0b47e1889efbef0eb57201700e5e2c4d9ee47f77
Nakiami <contact@greyboxconcepts.com.au>
2014-04-23 22:03:49 +1000

    Improved interest registration form

--- htdocs/interest.php
+++ htdocs/interest.php
@@ -7,14 +7,14 @@ prefer_ssl();
 head('Register interest');
 
 section_head('Register interest');
-message_inline_bland("The Mellivora team are likely to run more CTFs in the future. These will most likely be open to the public.
+message_inline_bland("We are likely to run more CTFs in the future. These will most likely be open to the public.
                       Input your email below if you're interested in hearing from us about future competitions.
                       We won't spam you. Your email address won't be shared with third parties.");
 
 form_start('actions/interest','form-signin');
 echo '
-    <input name="name" type="text" class="form-control" placeholder="Name / team name / nick">
-    <input name="email" type="text" class="form-control" placeholder="Email address">';
+    <input name="email" type="text" class="form-control" placeholder="Email address">
+    <input name="name" type="text" class="form-control" placeholder="Name / team name / nick">';
 
 if (CONFIG_RECAPTCHA_ENABLE) {
     display_captcha();
