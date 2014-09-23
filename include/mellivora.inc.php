552709db11b3bd41cff12f9dd90d3574d6213f57
Nakiami <contact@greyboxconcepts.com.au>
2014-09-23 19:03:14 +1000

    cleaned up layout structure

--- include/mellivora.inc.php
+++ include/mellivora.inc.php
@@ -4,12 +4,12 @@ require('config/config.inc.php');
 require(CONFIG_PATH_THIRDPARTY_COMPOSER . 'autoload.php');
 require(CONFIG_PATH_INCLUDE . 'session.inc.php');
 require(CONFIG_PATH_INCLUDE . 'general.inc.php');
-require(CONFIG_PATH_INCLUDE . 'layout/graphics.inc.php');
 require(CONFIG_PATH_INCLUDE . 'db.inc.php');
 require(CONFIG_PATH_INCLUDE . 'cache.inc.php');
 require(CONFIG_PATH_INCLUDE . 'json.inc.php');
 require(CONFIG_PATH_INCLUDE . 'email.inc.php');
 require(CONFIG_PATH_INCLUDE . 'files.inc.php');
+require(CONFIG_PATH_LAYOUT . 'layout.inc.php');
 
 session_set_cookie_params(
     CONFIG_SESSION_TIMEOUT,
