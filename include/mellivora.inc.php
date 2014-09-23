f4e9449c6c4d1af8fec36a0b5e67a45ee1c0acd6
Joshua Nelson <jonelson@atlassian.com>
2014-09-23 18:41:58 +1000

    Refactored graphics.inc.php to separate files

--- include/mellivora.inc.php
+++ include/mellivora.inc.php
@@ -4,7 +4,7 @@ require('config/config.inc.php');
 require(CONFIG_PATH_THIRDPARTY_COMPOSER . 'autoload.php');
 require(CONFIG_PATH_INCLUDE . 'session.inc.php');
 require(CONFIG_PATH_INCLUDE . 'general.inc.php');
-require(CONFIG_PATH_INCLUDE . 'graphics.inc.php');
+require(CONFIG_PATH_INCLUDE . 'layout/graphics.inc.php');
 require(CONFIG_PATH_INCLUDE . 'db.inc.php');
 require(CONFIG_PATH_INCLUDE . 'cache.inc.php');
 require(CONFIG_PATH_INCLUDE . 'json.inc.php');
