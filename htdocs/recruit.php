65718365d63b298737feda26fd4d96dbd9928f95
Nakiami <contact@greyboxconcepts.com.au>
2014-11-01 18:26:55 +1100

    clarifications on recruitment page

--- htdocs/recruit.php
+++ htdocs/recruit.php
@@ -9,7 +9,7 @@ enforce_authentication();
 head('Expression of interest');
 
 section_head('Expression of interest');
-message_inline_bland("Like the look of our sponsors? They're all hiring. Please fill out the form below if you wish to be contacted with recruitment information. Each team member can fill out the form individually. We won't share your details with anyone but our sponsors. We won't spam you.");
+message_inline_bland("Like the look of our sponsors? They're all hiring. Please fill out the form below if you wish to be contacted with recruitment information. Each team member can fill out the form individually. We won't share your details with anyone but our sponsors. We won't spam you. Only addresses entered into this form will be shared.");
 
 form_start('actions/recruit','form-signin');
 echo '
