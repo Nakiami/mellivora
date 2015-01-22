024fcc17ea3a59fe847f1f1c5a80c54d157a0c05
Nakiami <contact@greyboxconcepts.com.au>
2015-01-22 19:46:26 +1100

    caching on registration page

--- htdocs/register.php
+++ htdocs/register.php
@@ -23,6 +23,7 @@ if (CONFIG_ACCOUNTS_SIGNUP_ALLOWED) {
         <input name="',md5(CONFIG_SITE_NAME.'USR'),'" type="email" class="form-control" placeholder="Email address" required />
         ',(!CONFIG_ACCOUNTS_EMAIL_PASSWORD_ON_SIGNUP ? '<input name="'.md5(CONFIG_SITE_NAME.'PWD').'" type="password" class="form-control" placeholder="Password" required />' : '');
 
+    if (cache_start('register', CONFIG_CACHE_TIME_REGISTER)) {
         $user_types = db_select_all(
             'user_types',
             array(
@@ -44,19 +45,24 @@ if (CONFIG_ACCOUNTS_SIGNUP_ALLOWED) {
         }
 
         country_select();
+        cache_end('register');
+    }
 
-        if (CONFIG_RECAPTCHA_ENABLE_PUBLIC) {
-            display_captcha();
-        }
+    if (CONFIG_RECAPTCHA_ENABLE_PUBLIC) {
+        display_captcha();
+    }
 
-        echo '
-        <input type="hidden" name="action" value="register" />
-        <button class="btn btn-primary btn-lg" type="submit">Register team</button>
-    </form>
+    echo '
+    <input type="hidden" name="action" value="register" />
+    <button class="btn btn-primary btn-lg" type="submit">Register team</button>
+</form>
 ';
 
 } else {
-    echo '<i>Registration is currently closed, but you can still <a href="interest">register your interest for upcoming events</a>.</i>';
+    message_inline_blue(
+        'Sorry',
+        'Registration is currently closed, but you can still <a href="interest">register your interest for upcoming events</a>.'
+    );
 }
 
 foot();
