5d3932086e4643eb272c04352d79647e447a66bf
Nakiami <contact@greyboxconcepts.com.au>
2015-04-15 18:30:14 +1000

    do not allow admins to mark two submissions correct for one challenge

--- htdocs/admin/actions/list_submissions.php
+++ htdocs/admin/actions/list_submissions.php
@@ -30,6 +30,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     else if ($_POST['action'] == 'mark_correct') {
 
+        $submission = db_select_one(
+            'submissions',
+            array(
+                'user_id',
+                'challenge',
+                'correct'
+            ),
+            array(
+                'id'=>$_POST['id']
+            )
+        );
+
+        $num_correct_submissions = db_count_num(
+            'submissions',
+            array(
+                'user_id'=>$submission['user_id'],
+                'challenge'=>$submission['challenge'],
+                'correct'=>1
+            )
+        );
+
+        if ($num_correct_submissions > 0) {
+            message_error('This user already has a correct submission for this challenge');
+        }
+
         db_update('submissions', array('correct'=>1, 'marked'=>1), array('id'=>$_POST['id']));
 
         redirect(CONFIG_SITE_ADMIN_RELPATH . 'list_submissions.php?generic_success=1');
