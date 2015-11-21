55cc2dc3c1ce8275c8884372d3d0d60b8a4529b2
Nakiami <contact@greyboxconcepts.com.au>
2015-11-20 20:09:58 -0800

    do not redirect to actions

--- include/general.inc.php
+++ include/general.inc.php
@@ -356,6 +356,10 @@ function ends_with($haystack, $needle) {
 }
 
 function redirect ($url, $absolute = false) {
+    if (strpos($url, '/actions/') !== false) {
+        $url = CONFIG_INDEX_REDIRECT_TO;
+    }
+
     if (!$absolute) {
         $url = CONFIG_SITE_URL . trim($url, '/');
     }
