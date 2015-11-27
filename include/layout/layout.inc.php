d62bbb5bd6c098d4a064ad463d87fe9caed015ad
Joshua Nelson <jonelson@atlassian.com>
2015-11-27 16:57:25 +1100

    Implement improved success / failure messages

--- include/layout/layout.inc.php
+++ include/layout/layout.inc.php
@@ -131,6 +131,7 @@ function foot () {
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="',CONFIG_SITE_URL_STATIC_RESOURCES,'js/mellivora.js"></script>
+<script type="text/javascript" src="',CONFIG_SITE_URL_STATIC_RESOURCES,'js/fireworks.js"></script>
 
 </body>
 </html>';
