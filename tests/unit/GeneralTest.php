9bd01655c41ba5a9c6194910042fa82267b83a45
Nakiami <contact@greyboxconcepts.com.au>
2016-02-26 18:52:55 -0800

    add codeception and travis

---
+++ tests/unit/GeneralTest.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once(__DIR__.'/../../include/general.inc.php');
+
+class GeneralTest extends \Codeception\TestCase\Test {
+
+    public function testCutString() {
+        $this->assertEquals('aaa', cut_string('aaaaaa', 3));
+    }
+
+    public function testCutString_sameLength() {
+        $this->assertEquals('aaa', cut_string('aaa', 3));
+    }
+
+    public function testCutString_empty() {
+        $this->assertEquals('', cut_string('', 3));
+    }
+}
