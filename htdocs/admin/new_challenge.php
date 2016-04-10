2590d2cdcbd7cc9fae91596ba80183adf6d2de46
Nakiami <contact@greyboxconcepts.com.au>
2016-04-09 17:57:07 -0700

    clarify relies_on availability

--- htdocs/admin/new_challenge.php
+++ htdocs/admin/new_challenge.php
@@ -33,7 +33,7 @@ $opts = db_query_fetch_all('
     ORDER BY ca.title, ch.title'
 );
 
-array_unshift($opts, array('id'=>0, 'title'=> '-- User must solve selected challenge before revealing this one --'));
+array_unshift($opts, array('id'=>0, 'title'=> '-- This challenge will become available after the selected challenge is solved (by any user) --'));
 
 form_select($opts, 'Relies on', 'id', $challenge['relies_on'], 'title', 'category');
 
