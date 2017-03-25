f8a0c160ede33ec960caa9fdb841f7921fce780a
Nakiami <contact@greyboxconcepts.com.au>
2017-03-26 08:42:32 +1100

    fix xss in admin list news title

--- htdocs/admin/list_news.php
+++ htdocs/admin/list_news.php
@@ -12,7 +12,7 @@ $news = db_query_fetch_all('SELECT * FROM news ORDER BY added DESC');
 foreach($news as $item) {
     echo '
         <div class="news-container">';
-            section_head($item['title'] . ' <a href="edit_news.php?id='.htmlspecialchars($item['id']).'" class="btn btn-xs btn-primary">Edit</a>', '', false);
+            section_head(htmlspecialchars($item['title']) . ' <a href="edit_news.php?id='.htmlspecialchars($item['id']).'" class="btn btn-xs btn-primary">Edit</a>', '', false);
     echo '
         <div class="news-body">
                 ',get_bbcode()->parse($item['body']),'
