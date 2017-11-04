1f200feb36c0de6dec8a0dc6af87368f0603db2b
Nakiami <contact@greyboxconcepts.com.au>
2017-11-05 10:53:39 +1100

    show 0% solvers rather than 100 if no active participants

--- include/layout/scores.inc.php
+++ include/layout/scores.inc.php
@@ -99,7 +99,7 @@ function challenges($categories) {
                 </td>
 
                 <td class="center">
-                    ',number_format($num_participating_users ? ($num_solvers / $num_participating_users) : 1 * 100),'%
+                    ',number_format($num_participating_users ? ($num_solvers / $num_participating_users) : 0 * 100),'%
                 </td>
 
                 <td class="team-name">';
