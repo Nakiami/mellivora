f8a3109b3261ec6e79a79bdb7bd7dcc1919ab5b4
Nakiami <contact@greyboxconcepts.com.au>
2018-05-01 10:15:54 +1000

    wip

--- include/config_loader.inc.php
+++ include/config_loader.inc.php
@@ -9,10 +9,10 @@ require('Config.php');
 require('config/config.default.inc.php');
 require('config/db.default.inc.php');
 
-if (is_file('config/config.inc.php')) {
+if (is_file(dirname(__FILE__) . '/config/config.inc.php')) {
     require('config/config.inc.php');
 }
 
-if (is_file('config/db.inc.php')) {
+if (is_file(dirname(__FILE__) . '/config/db.inc.php')) {
     require('config/db.inc.php');
 }
